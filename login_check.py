1d773651160906d81b85e214cdbd2d8faf6df263
gecexgokyuzu <103609940+gecexgokyuzu@users.noreply.github.com>
2022-04-27 17:51:02 +0300

    changed user agent generator(currently does not work), proxy gets caught as well. will work on this tomorrow

--- login_check.py
+++ login_check.py
@@ -3,29 +3,29 @@ import autoit
 import os.path
 import random
 import undetected_chromedriver as uc
+import ua_generator
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as expect
 from selenium.webdriver.support.wait import WebDriverWait
 from chrome_proxy_extension import ProxyExtension
-from user_agent import generate_user_agent
+from selenium_stealth import stealth
 
-#get usernames and passwords in this list. format="username|password"
+# get usernames and passwords in this list. format="username|password"
 
 userName_passWord = []
 
 with open(os.path.dirname(__file__) + "/../facebook_accounts.txt", "r") as file:
     userName_passWord = file.readlines()
 
-userNames = [] 
+userNames = []
 passWords = []
 accountCount = 0
 onlineAccounts = []
-
 ##-------------------------------------------------------------------------------------------##
 
-#Setting up proxy:
+# Setting up proxy:
 
-with open (os.path.dirname(__file__) + "/../IPRoyal_TR_rotating.txt", "r") as proxyCredentials:
+with open(os.path.dirname(__file__) + "/../IPRoyal_TR_rotating.txt", "r") as proxyCredentials:
     proxyRaw = proxyCredentials.read()
     proxyCredentials.close()
 
@@ -36,20 +36,26 @@ proxyPort = splitter[1]
 proxyUser = splitter[2]
 proxyPass = splitter[3]
 
-proxy = (proxyIp, int(proxyPort), proxyUser, proxyPass) #proxy with auth
+proxy = (proxyIp, int(proxyPort), proxyUser, proxyPass)  # proxy with auth
 proxy_extension = ProxyExtension(*proxy)
+
 
 def SetProxy_SetUserAgent():
     options = uc.ChromeOptions()
     options.add_argument(f"--load-extension={proxy_extension.directory}")
-    agent = generate_user_agent(os="win", navigator="chrome")
-    options.add_argument(f"--user-agent={agent}")
     options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-infobars")
+    useragent = ua_generator.generate(device='desktop', browser='chrome', platform='win32')
+    options.add_argument(f"user-agent={useragent}")
+    options.set_capability('unhandledPromptBehavior', 'dismiss')
+    options.set_capability('pageLoadStrategy', 'none')
     return options
 
 ##-------------------------------------------------------------------------------------------##
 
-#split the string into username and password
+# split the string into username and password
+
 
 for line in userName_passWord:
     splitter = line.split('|')
@@ -57,6 +63,7 @@ for line in userName_passWord:
     passWords.append(splitter[1].strip())
 
 # log keeping function
+
 
 def run_log(userName, passWord, isActive, accountCount):
     if isActive == True:
@@ -69,20 +76,27 @@ def run_log(userName, passWord, isActive, accountCount):
 
 # random time.sleep function
 
+
 def wait():
     time.sleep(round(random.random.uniform(0.5, 2), 2))
 
 ##-------------------------------------------------------------------------------------------##
 
-#Main App:
+# Main App:
+
 
 if __name__ == "__main__":
     for line in userNames:
-        driver = uc.Chrome(options=SetProxy_SetUserAgent())
-        driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-        driver.get("https://whatismyipaddress.com/")
+        driver = uc.Chrome(options=SetProxy_SetUserAgent()[0])
+        driver.execute_script(
+            "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        # ------------- STEALTH DRIVER ------------- #
+        stealth(driver, languages=["en-US", "en"], vendor="Google Inc.", platform="Win32", webgl_vendor="Intel Inc.",
+                renderer="Intel Iris OpenGL Engine", fix_hairline=False, hardware_concurrency=4, run_on_insecure_origins=False)
+        driver.get("https://bot.incolumitas.com/")
+        time.sleep(250)
         try:
-            time.sleep(3,14)
+            time.sleep(3, 14)
 
             email_input = driver.find_element(By.ID, "email")
             email_input.click()
@@ -94,7 +108,7 @@ if __name__ == "__main__":
             password_input.click()
 
             wait()
-            
+
             autoit.send(passWords[accountCount])
             login_btn = driver.find_element(By.NAME, "login")
 
@@ -102,8 +116,8 @@ if __name__ == "__main__":
 
             login_btn.click()
             logincheck = WebDriverWait(driver, 5).until(
-                expect.element_to_be_clickable((By.XPATH, "//div[@aria-label='Hesap']"))) 
-                #(Hesap means account in turkish, get the xpath again if you are using another lang option)
+                expect.element_to_be_clickable((By.XPATH, "//div[@aria-label='Hesap']")))
+            # (Hesap means account in turkish, get the xpath again if you are using another lang option)
 
             wait()
 
@@ -114,7 +128,7 @@ if __name__ == "__main__":
             try:
                 logincheck = WebDriverWait(driver, 5).until(
                     expect.element_to_be_clickable((By.XPATH, "//div[@aria-label='Hesap']")))
-                
+
                 driver.quit()
 
                 run_log(line, passWords[accountCount], True, accountCount)
