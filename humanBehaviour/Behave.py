58992f67bf76de37420c0a34492c59e9ae93c274
gecexgokyuzu <103609940+gecexgokyuzu@users.noreply.github.com>
2022-05-13 21:14:11 +0300

    Trying to add humanlike mouse movements (curved and randomly timed, start-stop speed etc.)

--- humanBehaviour/Behave.py
+++ humanBehaviour/Behave.py
@@ -3,6 +3,12 @@ import random
 import autoit
 import undetected_chromedriver
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.action_chains import ActionChains
+import numpy as np
+import scipy.interpolate as si
+
+# Screen Width(X) = 1536-1
+# Screen Height(Y) = 864-1
 
 
 def TranslateCoordinates(xy: WebElement):
@@ -10,10 +16,16 @@ def TranslateCoordinates(xy: WebElement):
     listedXY[1] = listedXY[1] + 74
     return listedXY
 
-def RandomMovement():
-    x = random(500,1000)
-    y = random(500,1000)
-    autoit.mouse_move(x,y)
+def RandomMovement(isLoad = False, iCount: int = 1):
+    if not isLoad:
+        x = random(500,1000)
+        y = random(500,1000)
+        z = random(10,30)
+        autoit.mouse_move(x,y,z)
+    else:
+        centerX = 750
+        centerY = 430
+        iCount += 1
 
 def is_element_visible_in_viewpoint(driver, element) -> bool:
     return driver.execute_script("var elem = arguments[0],                 " 
@@ -28,8 +40,12 @@ def is_element_visible_in_viewpoint(driver, element) -> bool:
                                  "return false;                            "
                                  , element)
 
-def NaturalWait(min: float, max: float, isLoad=False):
+def NatWait(isLoad=False, min: float = 0.5, max: float = 2):
     if isLoad:
-        time.sleep(round(random.uniform(min, max),3))
+        time.sleep(round(random.uniform(min, max), 4))
     else:
-        time.sleep
+        time.sleep(round(random.uniform(min, max), 3))
+
+def NatWaitLoad(driver: undetected_chromedriver.Chrome, element: WebElement):
+    while not is_element_visible_in_viewpoint(driver, element):
+        NatWait(True, 0.02, 0.05)
